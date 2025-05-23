1fde725281d67f879f02b07d0f2978c8383968a1
Rohit Rastogi <rohit@typedef.ai>
2025-05-23 13:30:01 -0700

    meh

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,9 +2,15 @@ pub mod expressions;
 
 use pyo3::prelude::*;
 
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
+}
+
 /// Python module implemented in Rust
 #[pymodule]
 fn _polars_plugins(_py: Python<'_>, _m: &Bound<'_, PyModule>) -> PyResult<()> {
+    _m.add_function(wrap_pyfunction!(sum_as_string, _m)?)?;
     Ok(())
 }
 
---
+++ src/test_proj/polars_plugins/sum.py
@@ -0,0 +1,3 @@
+from test_proj._polars_plugins import sum_as_string
+
+print(sum_as_string(1, 2))
